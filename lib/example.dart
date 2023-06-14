4c2594e9939366c5abea30665d66eee00b99487c
Miracle-Blue <miracle.blue.com@gmail.com>
2023-06-14 07:01:07 +0500

    media_query

--- lib/example.dart
+++ lib/example.dart
@@ -1,53 +1,75 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
-
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class HomePageState extends State<HomePage> {
-  void tap() {
-    
-    text2 = text1;
-    setState(() {});
+class _HomePageState extends State<HomePage> {
+  late MediaQueryData  data;
+  final FocusNode f = FocusNode();
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    data = MediaQuery.of(context);
   }
 
-  String text1 = 'void func';
-  String text2 = 'state less';
+  @override
+  void dispose() {
+    f.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ButtonStateless(),
-            const SizedBox(height: 20),
-            ButtonStateless(),
-            const SizedBox(height: 20),
-            const ButtonStateless(),
-          ],
+    return GestureDetector(
+      onTap: () {
+        if (f.hasFocus) {
+          FocusScope.of(context).unfocus();
+        } else {
+          FocusScope.of(context).requestFocus(f);
+        }
+      },
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  focusNode: f,
+                  controller: TextEditingController()..text = "padding: ${data.padding}",
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                  textAlign: TextAlign.center,
+                  showCursor: false,
+                ),
+                TextField(
+                  controller: TextEditingController()..text = "viewInsets: ${data.viewInsets}",
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                  textAlign: TextAlign.center,
+                  showCursor: false,
+                ),
+                TextField(
+                  controller: TextEditingController()..text = "viewPadding: ${data.viewPadding}",
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                  textAlign: TextAlign.center,
+                  showCursor: false,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
-    );
-  }
-}
-
-class ButtonStateless extends StatelessWidget {
-  const ButtonStateless({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final stateHomePage = context.findAncestorStateOfType<HomePageState>();
-    return ElevatedButton(
-      onPressed: () => stateHomePage?.tap(),
-      child: Text(stateHomePage?.text2 ?? ''),
     );
   }
 }
